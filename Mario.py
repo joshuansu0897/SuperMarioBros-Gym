f8191e40f4515f345bd01b6a56e71c6d2d406b2c
Joshua Saucedo <joshuansu0897@gmail.com>
2018-12-21 11:23:16 -0700

    1000 juegos

--- Mario.py
+++ Mario.py
@@ -4,12 +4,14 @@ import numpy as np
 
 env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
 
-observation = env.reset()
-done = False
-t = 0
-while not done:
-    action = np.random.randint(2, size=6)
-    observation, reward, done, info = env.step(action)
-    t += 1
-    if not t % 100:
-        print(t, info)
+
+for _ in range(1000):
+    observation = env.reset()
+    done = False
+    t = 0
+    while not done:
+        action = np.random.randint(2, size=6)
+        observation, reward, done, info = env.step(action)
+        t += 1
+        if not t % 100:
+            print(t, info)
