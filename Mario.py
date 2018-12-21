1c822652a57446bd288f1d2bddb3f76d40bf4d0e
Joshua Saucedo <joshuansu0897@gmail.com>
2018-12-21 10:50:51 -0700

    stupid mario

--- Mario.py
+++ Mario.py
@@ -1,12 +1,14 @@
 import gym
 import ppaquette_gym_super_mario
+import numpy as np
 
 env = gym.make('ppaquette/SuperMarioBros-1-1-v0')
+
 observation = env.reset()
 done = False
 t = 0
 while not done:
-    action = env.action_space.sample()
+    action = np.random.randint(2, size=6)
     observation, reward, done, info = env.step(action)
     t += 1
     if not t % 100:
